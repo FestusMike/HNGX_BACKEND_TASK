9a6c1986484cf5d963beabb1a3e3ee0d678d7e86
FestusMike <festusmike98@gmail.com>
2023-09-07 12:01:28 +0100

    Minor Corrections

--- json_endpoint.py
+++ json_endpoint.py
@@ -4,7 +4,7 @@ import datetime
 app = Flask(__name__)
 
 @app.route('/get_info_in_json', methods=['GET'])
-def get_info():
+def get_info_in_json():
     slack_name = request.args.get('slack_name')
     track = request.args.get('track')
 
@@ -12,18 +12,16 @@ def get_info():
         return jsonify({"error": "slack_name and track are required parameters"}), 400
 
     current_day = datetime.datetime.utcnow().strftime('%A')
-    current_utc_time = datetime.datetime.utcnow() + datetime.timedelta(hours=2)
+    utc_time = datetime.datetime.utcnow() + datetime.timedelta(hours=2)
 
-    github_url_file = "https://github.com/yourusername/yourrepository/blob/main/yourscript.py"
-    github_url_source = "https://github.com/yourusername/yourrepository"
-
+    
     response_data = {
         "slack_name": "Micheal_Arifajogun",
         "current_day": current_day,
-        "utc_time": current_utc_time.strftime('%Y-%m-%d %H:%M:%S'),
+        "utc_time": utc_time.strftime('%Y-%m-%d %H:%M:%S'),
         "track": "backend",
-        "github_file_url": github_url_file,
-        "github_repo_url": github_url_source,
+        "github_file_url": "https://github.com/FestusMike/HNGX_BACKEND_TASK/blob/main/json_endpoint.py",
+        "github_repo_url": "https://github.com/FestusMike/HNGX_BACKEND_TASK/tree/main",
         "status_code": 200
     }
 
